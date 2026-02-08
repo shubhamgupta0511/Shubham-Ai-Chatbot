22d0f6751bd23829bcd9e40e45c4a1ded5610bb6
Shubham Gupta <shubham320014@gmail.com>
2026-02-09 01:22:43 +0530

    c2

--- app.py
+++ app.py
@@ -102,3 +102,8 @@ def chat():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+if __name__ == "_main_":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
